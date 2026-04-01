bd567c4c572f9ce1b9ab92ea6d3f9316ede4471d
park <parkharnew45@gmail.com>
2026-04-01 11:02:27 +0530

    commit 8

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
