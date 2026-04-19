d3c07386c66ff8997c60411a3e880ec343a9bae1
prakharsf27 <98946448+prakharsf27@users.noreply.github.com>
2026-04-19 15:46:00 +0530

    feat: integrate vercel analytics

--- src/main.jsx
+++ src/main.jsx
@@ -13,6 +13,7 @@ createRoot(document.getElementById("root")).render(
       <ThemeProvider>
         <BrowserRouter>
           <App />
+          <Analytics />
         </BrowserRouter>
       </ThemeProvider>
     </AuthProvider>
